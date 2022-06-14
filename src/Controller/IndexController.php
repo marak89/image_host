7cdf3e09d25f9bfb325af765b0c7167ebfa88a27
Marcin Rał <marak89@marak89.com>
2022-06-14 15:22:37 +0200

    create upload photo Form

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\UploadPhotoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,10 @@ class IndexController extends AbstractController
      */
     public function index(): Response
     {
+
+        $form = $this->createForm(UploadPhotoType::class);
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'form' => $form->createView(),
         ]);
     }
 }
