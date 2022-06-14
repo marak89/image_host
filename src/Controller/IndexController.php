501afb45a08e2b88098a6be6227f4c1d1b0d5a58
Marcin Rał <marak89@marak89.com>
2022-06-14 15:53:35 +0200

    ready save file to database/filesystem

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Photo;
 use App\Form\UploadPhotoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +14,26 @@ class IndexController extends AbstractController
     /**
      * @Route("/", name="app_index")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
 
         $form = $this->createForm(UploadPhotoType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            if($this->getUser()){
+                $em = $this->getDoctrine()->getManager();
+                $entityPhoto = new Photo();
+                $entityPhoto->setFilename($form->get('filename')->getData());
+                $entityPhoto->setIsPublic($form->get('is_public')->getData());
+                $entityPhoto->setUploadedAt(new \DateTimeImmutable());
+                $entityPhoto->setUser($this->getUser());
+
+                $em->persist($entityPhoto);
+                $em->flush();
+            }
+        }
+
         return $this->render('index/index.html.twig', [
             'form' => $form->createView(),
         ]);
